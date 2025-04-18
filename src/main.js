d63fb501015a21eaf652c09a6bba7573a5360230
atmoner <contact.atmoner@gmail.com>
2025-04-18 18:05:16 +0200

    Refactor code structure for improved readability and maintainability

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createPinia } from 'pinia' 
+
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +10,6 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+app.use(router) 
 
 app.mount('#app')
